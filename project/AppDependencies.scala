39f59a91172a159a4cd3fab5dbf975a30b034e38
ed1286 <48755019+ed1286@users.noreply.github.com>
2025-07-18 09:45:43 +0100

    NGR-hello-world-et-al -version bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.16.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.6.0"
